2b327994eff79622268e93f0bb2ab80a234191cf
benyou1969 <benyou.11@hotmail.com>
2020-05-06 22:05:25 +0100

    Signing a JWT Token Upon Authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
-@Controller('auth') 
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signup')
@@ -12,7 +12,9 @@ export class AuthController {
     return this.authService.signUp(authCredentialsDto);
   }
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
