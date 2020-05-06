ba82c36c4710181928f65fb0a80f581ad2652263
benyou1969 <benyou.11@hotmail.com>
2020-05-06 20:26:27 +0100

    Error Handling depulcate username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,14 +1,27 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = new User();
+
     user.username = username;
     user.password = password;
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === "23505") {
+        throw new ConflictException(`${username} already exists`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
