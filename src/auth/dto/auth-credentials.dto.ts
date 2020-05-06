0a060a8aeb8b8fe185bc82ab1b1d6527ea2686e0
benyou1969 <benyou.11@hotmail.com>
2020-05-06 08:14:26 +0100

    Validation AuthCredentialsDto, Password strength

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\w+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/ , { message: 'Password too weak'})
   password: string;
 }
